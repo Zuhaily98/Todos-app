c3782eb9791abe99b2586b411f2ba3d471d7c20e
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2020-10-11 15:53:32 +0800

    add navigation bar to layouts for all the views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,24 @@
 </head>
 
 <body>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">Todos App</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/todos">todos <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            
+        </div>
+    </nav>
+
     <div class="container">
         @yield('content')
     </div>
