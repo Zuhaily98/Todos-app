1b51aa9eb5258093a3b6582d547f796e443a06d7
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2020-10-11 15:33:40 +0800

    add layouts for views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+
+    <title>Todos</title>
+
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,58 +1,36 @@
-<!DOCTYPE html>
+@extends('layouts.app')
 
-<html lang="en">
+@section('content')
 
-<head>
+<h1 class="text-center my-5">Todos Page</h1> <!-- my-5 means margin y of 5 -->
 
-    <meta charset="UTF-8">
+<div class="row-justify-content-center">
+    <!-- .row-justify-content-center -->
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div class="col-md-8 offset-md-2">
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <div class="card card-default">
+            <!-- .card.card-default -->
+            <div class="card-header">
+                <!-- .card-header -->
+                Todos
+            </div>
 
-    <!-- CSS only -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+            <div class="card-body">
+                <!-- .card-body -->
+                <ul class="list-group">
+                    @foreach($todos as $todo)
+                    <li class="list-group-item">
+                        {{ $todo->name }}
 
-    <title>Todos</title>
-
-</head>
-
-<body>
-    <!-- .container -->
-    <div class="container">
-
-        <h1 class="text-center my-5">Todos Page</h1> <!-- my-5 means margin y of 5 -->
-        
-        <div class="row-justify-content-center">
-            <!-- .row-justify-content-center -->
-
-            <div class="col-md-8 offset-md-2">
-
-                <div class="card card-default">
-                    <!-- .card.card-default -->
-                    <div class="card-header">
-                        <!-- .card-header -->
-                        Todos
-                    </div>
-
-                    <div class="card-body">
-                        <!-- .card-body -->
-                        <ul class="list-group">
-                            @foreach($todos as $todo)
-                            <li class="list-group-item">
-                                {{ $todo->name }}
-
-                                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
-                                 <!-- button.btn.btn-primary -->
-                            </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
+                        <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                        <!-- button.btn.btn-primary -->
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
+</div>
 
-</body>
-
-</html>
+@endsection
